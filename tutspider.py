e7822cbb0ce5b6eee7867b4a6aef7e0631350e43
DJM19 <32272299+DJM19@users.noreply.github.com>
2017-10-04 23:56:49 +0530

    Add files via upload
    added csv file to drop box

--- tutspider.py
+++ tutspider.py
@@ -1,3 +1,4 @@
+import dropbox  
 from scrapy.spiders import Spider
 from scrapy.selector import Selector
 from tutsplus.items import TutsplusItem
@@ -11,9 +12,10 @@ class MySpider(Spider):
 
   def parse(self, response):
     items = []
-    items1 = []
     sel = Selector(response)
-    
+    sites = sel.xpath('.//div[@class="l-tile__item"]')
+
+
     for site in sel.xpath('.//div[@class="l-tile__item"]'):
       item = TutsplusItem()
       item["title"] = site.xpath('.//a[@class="c-brick-hotel-booking__link"]/h4/text()').extract()
@@ -22,10 +24,14 @@ class MySpider(Spider):
       item["price"] = site.xpath('.//div[@class="c-brick-hotel-booking__price-box"]//span[@class="o-price__num"]/text()').extract()
       items.append(item)
       yield item
-    next_page = response.css('li.next a::attr(href)').extract_first()
+
+    next_page = response.css('li.next a::attr(href)').extract_first() 
     if next_page is not None:
-      next_page = response.urljoin(next_page)
-      yield scrapy.Request(next_page, callback=self.parse)
+     next_page = response.urljoin(next_page)
+     yield scrapy.Request(next_page, callback=self.parse)
 
-
+dbx = dropbox.Dropbox('QOHUZH3iigAAAAAAAAABQZVZ5bHmCNXt4kwcxGoN2tTRdZjCiecAzBZ6TAsvJ5s9')
+dbx.users_get_current_account()
+with open("C:/Users/DELL STORE/tutsplus/tutsplus/ws.csv", "rb") as f:
+    dbx.files_upload(f.read(), '/ws.csv', mute = True)
 
